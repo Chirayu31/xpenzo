ac8b945186f469cf048f871a7529c0320fcd020c
Chirayu31 <chirayupancholi17@gmail.com>
2025-02-17 21:08:24 +0530

    fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,15 +7,5 @@ export default withAuth({
 })
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for:
-     * - root (/) - should be public
-     * - API routes (/api/...)
-     * - Next.js static files
-     * - Next.js image optimization
-     * - Common metadata files
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|$).*)',
-  ],
+  matcher: ['/transaction/:path*', '/category/:path*', '/logout'],
 }
